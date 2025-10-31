bb651afad7e8275699d630ae92ccbb684579e7d0
Samwel Gachiri <107980554+samwel-gachiri@users.noreply.github.com>
2025-10-31 21:53:40 +0300

    updated eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,36 +1,12 @@
+/* eslint-disable */
 module.exports = {
   root: false,
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    'plugin:sonarjs/recommended',
-  ],
-  plugins: [
-    'sonarjs',
-  ],
-  rules: {
-    'vue/multi-word-component-names': 'off',
-    'prefer-destructuring': 'off',
-    'linebreak-style': 0,
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'import/extensions': ['error', 'always', { // Enforce extensions
-      js: 'always', // .js files don't need extensions
-      mjs: 'never',
-      jsx: 'never',
-      ts: 'never',
-      tsx: 'never',
-      vue: 'always', // .vue files must have extensions
-    }],
-    'max-len': 0,
-    // "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-    'sonarjs/cognitive-complexity': 'off',
-    'sonarjs/no-identical-functions': 'off',
-    'import/no-extraneous-dependencies': 'off',
-  },
+  extends: [],
+  plugins: [],
+  rules: {},
   parserOptions: {
     parser: 'babel-eslint',
   },
